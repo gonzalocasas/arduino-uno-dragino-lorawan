44e9ec7a7b625f907835790f3b0d43a572b1390b
Gonzalo Casas <gonzalo.casas@local.ch>
2016-09-10 22:02:14 +0200

    Reduce the Hello World sample to the absolute minimum

--- hello-world/hello-world.ino
+++ hello-world/hello-world.ino
@@ -1,6 +1,5 @@
 /*******************************************************************************
  * Do not forget to define the radio type correctly in config.h.
- *
  * Credit: based on the work by Thomas Telkamp and Matthijs Kooijman 
  *******************************************************************************/
 
@@ -12,9 +11,12 @@
  * TODO: CHANGE THE FOLLOWING KEYS
  *************************************/
 // LoRaWAN keys (NwkSKey: network session key, AppSKey, application session key and DevAddr, end-device address)
-static const PROGMEM u1_t NWKSKEY[16] = {0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##};
-static const PROGMEM u1_t APPSKEY[16] = {0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##};
+static const u1_t NWKSKEY[16] = {0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##};
+static const u1_t APPSKEY[16] = {0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##, 0x##};
 static const u4_t DEVADDR = 0x########;
+
+// Payload to send (uplink)
+static uint8_t mydata[] = "hi";
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
@@ -23,7 +25,6 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "hi";
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
@@ -39,15 +40,13 @@ const lmic_pinmap lmic_pins = {
 };
 
 void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
     switch(ev) {
         case EV_TXCOMPLETE:
             Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
             if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
+              Serial.println(F("Received ACK"));
             if (LMIC.dataLen) {
-              Serial.println(F("Received "));
+              Serial.println(F("Received downlink with"));
               Serial.println(LMIC.dataLen);
               Serial.println(F(" bytes of payload"));
             }
@@ -67,57 +66,23 @@ void do_send(osjob_t* j){
     } else {
         // Prepare upstream data transmission at the next possible time.
         LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
-        Serial.println(F("Packet queued"));
+        Serial.println(F("Sending uplink packet..."));
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
     Serial.begin(115200);
-    Serial.println(F("Starting"));
+    Serial.println(F("Starting..."));
 
     // LMIC init
     os_init();
+
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC_reset();
 
     // Set static session parameters.
-    uint8_t appskey[sizeof(APPSKEY)];
-    uint8_t nwkskey[sizeof(NWKSKEY)];
-    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
-    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
-    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
-
-    #if defined(CFG_eu868)
-    // Set up the channels used by the Things Network, which corresponds
-    // to the defaults of most gateways. Without this, only three base
-    // channels from the LoRaWAN specification are used, which certainly
-    // works, so it is good for debugging, but can overload those
-    // frequencies, so be sure to configure the full frequency range of
-    // your network here (unless your network autoconfigures them).
-    // Setting up channels should happen after LMIC_setSession, as that
-    // configures the minimal channel set.
-    // NA-US channels 0-71 are configured automatically
-    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
-    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
-    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
-    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
-    // devices' ping slots. LMIC does not have an easy way to define set this
-    // frequency and support for class B is spotty and untested, so this
-    // frequency is not configured here.
-    #elif defined(CFG_us915)
-    // NA-US channels 0-71 are configured automatically
-    // but only one group of 8 should (a subband) should be active
-    // TTN recommends the second sub band, 1 in a zero based count.
-    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
-    LMIC_selectSubBand(1);
-    #endif
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
 
     // Disable link check validation
     LMIC_setLinkCheckMode(0);
